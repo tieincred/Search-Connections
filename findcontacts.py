9334f296dcfe4e8e2fcf22874ae0b712acdb56ac
Tausif Iqbal <60173762+tieincred@users.noreply.github.com>
2021-08-22 20:39:14 +0530

    Update findcontacts.py

--- findcontacts.py
+++ findcontacts.py
@@ -23,14 +23,14 @@ class extract:
         display.start()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
+#         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("window-size=1920x1080")
         chrome_options.add_argument("--enable-javascript")
-#         wd = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        wd = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-        wd = webdriver.Chrome('chromedriver',options=chrome_options)
+#         wd = webdriver.Chrome('chromedriver',options=chrome_options)
         login_link = 'https://www.linkedin.com/uas/login'
         wd.get(login_link)
         time.sleep(10)
